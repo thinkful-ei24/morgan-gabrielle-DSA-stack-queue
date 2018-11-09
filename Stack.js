f3234f362a7266ae03be466169541bbdb6cddced
Gabriel Morris <gabemorris@outlook.com>
2018-11-09 09:01:15 -0800

    push, pop, display, peek

--- Stack.js
+++ Stack.js
@@ -1,7 +1,51 @@
+const { peek, display } = require('./helpers');
+
 class Node {
-
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
-export default class Stack {
-  
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new Node(data, null);
+      return this;
+    }
+
+    const node = new Node(data, this.top);
+    this.top = node;
+
+    return this;
+  }
+
+  pop() {
+    const node = this.top;
+    this.top = node.next;
+
+    return node.data;
+  }
 }
+
+function main() {
+  const starTrek = new Stack();
+
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+  starTrek.pop();
+
+  peek(starTrek);
+  console.log('====');
+  display(starTrek);
+}
+
+main();
+
+exports.default = Stack;
--- helpers.js
+++ helpers.js
@@ -1,6 +1,23 @@
-function peek() {}
+function peek(stack) {
+  if (!stack.top) {
+    return null;
+  }
 
-function display() {}
+  console.log(stack.top.data);
+}
+
+function display(stack) {
+  let counter = stack.top;
+
+  if (!stack.top) {
+    return null;
+  }
+
+  while (counter) {
+    console.log(counter.data);
+    counter = counter.next;
+  }
+}
 
 module.exports = {
   peek,
